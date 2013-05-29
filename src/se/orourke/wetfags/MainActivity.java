253f812874656be243c9fe9d2b1414c59094c2ce
Kevin O'Rourke <kevin@caboose.org.uk>
2013-05-29 19:56:35 +0200

    Now has multiple swipable pages.
    Also fixed some minor bugs and now allow rotation.

--- src/se/orourke/wetfags/MainActivity.java
+++ src/se/orourke/wetfags/MainActivity.java
@@ -3,9 +3,12 @@ package se.orourke.wetfags;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.KeyEvent;
-import android.view.Menu;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -14,32 +17,71 @@ import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
 public class MainActivity extends Activity implements OnCheckedChangeListener, OnEditorActionListener {
+	private class MyPagerAdapter extends PagerAdapter {
+
+		@Override
+		public int getCount() {
+			return 2;
+		}
+
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1) {
+			return arg0 == ((View)arg1);
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			((ViewPager) container).removeView((View) object);
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+            LayoutInflater inflater = (LayoutInflater) container.getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            int resId = 0;
+            switch (position) {
+	            case 0:
+	                resId = R.layout.wetfags_layout;
+	                break;
+	            case 1:
+	                resId = R.layout.parameters_layout;
+	                break;
+            }
+            View view = inflater.inflate(resId, null);
+            ((ViewPager) container).addView(view, 0);
+            return view;
+        }
+		
+	}
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		MyPagerAdapter adapter = new MyPagerAdapter();
+	    ViewPager myPager = (ViewPager) findViewById(R.id.resultspanelpager);
+	    myPager.setAdapter(adapter);
+	    myPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				super.onPageSelected(position);
+				updateValues();
+			}
+	    });
+	    
 		RadioGroup monthYearKgGroup = (RadioGroup) findViewById(R.id.monthsYearsKgRadioGroup);
 		monthYearKgGroup.setOnCheckedChangeListener(this);
 		EditText weightAgeEditText = (EditText) findViewById(R.id.weightAgeEditText);
 		weightAgeEditText.setOnEditorActionListener(this);
 	}
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		updateValues();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	
 	/** Updates the displayed values */
 	private void updateValues() {
+		if (findViewById(R.id.w_formula) == null)
+			return;
+		
 		EditText weightAgeEditText = (EditText) findViewById(R.id.weightAgeEditText);
 		int value = -1;
 		try {
@@ -78,9 +120,25 @@ public class MainActivity extends Activity implements OnCheckedChangeListener, O
 					weight = value;
 				break;
 		}
-
+		
+		ViewPager myPager = (ViewPager) findViewById(R.id.resultspanelpager);
+		switch (myPager.getCurrentItem())
+		{
+			case 0:
+				updateWetfagsValues(weight, age);
+				break;
+			
+			case 1:
+				updateParametersValues(weight, age);
+				break;
+		}
+	}
+	
+	private void updateWetfagsValues(int weight, double age) {
+		if (findViewById(R.id.wetfags_rel_layout) == null)
+			return;
+		
 		if (weight == -1) {
-			// show the unknown value for all of them
 			setTextViewText(R.id.w_value, R.string.unknown_val);
 			setTextViewText(R.id.e_value, R.string.unknown_val);
 			setTextViewText(R.id.f_value, R.string.unknown_val);
@@ -89,45 +147,75 @@ public class MainActivity extends Activity implements OnCheckedChangeListener, O
 			setTextViewText(R.id.s_iv_value, R.string.unknown_val);
 			setTextViewText(R.id.s_pr_value, R.string.unknown_val);
 		} else {
-			// recalculate and display
-			setTextViewString(R.id.w_value, String.format(getString(R.string.w_format), weight));
+			setTextViewText(R.id.w_value, String.format(getString(R.string.w_format), weight));
 			
 			int energy = 4*weight;
-			setTextViewString(R.id.e_value, String.format(getString(R.string.e_format), energy));
+			setTextViewText(R.id.e_value, String.format(getString(R.string.e_format), energy));
 			
 			if (age < 0.0)
 				setTextViewText(R.id.t_value, R.string.unknown_val);
 			else
 			{
 				double tube = (age/4.0) + 4.0;
-				setTextViewString(R.id.t_value, String.format(getString(R.string.t_format), Math.round( tube * 2.0 ) / 2.0));
+				setTextViewText(R.id.t_value, String.format(getString(R.string.t_format), Math.round( tube * 2.0 ) / 2.0));
 			}
 			
 			int fluids_low = 10*weight;
 			int fluids_high = 20*weight;
-			setTextViewString(R.id.f_value, String.format(getString(R.string.f_format), fluids_low, fluids_high));
+			setTextViewText(R.id.f_value, String.format(getString(R.string.f_format), fluids_low, fluids_high));
 			
 			double adrenaline_ug = 0.010*weight; // mg
 			double adrenaline_ml = adrenaline_ug / 0.1; // 0.1 mg/ml
-			setTextViewString(R.id.a_value, String.format(getString(R.string.a_format), adrenaline_ml));
+			setTextViewText(R.id.a_value, String.format(getString(R.string.a_format), adrenaline_ml));
 			
 			int glucose = 2*weight;
-			setTextViewString(R.id.g_value, String.format(getString(R.string.g_format), glucose));
+			setTextViewText(R.id.g_value, String.format(getString(R.string.g_format), glucose));
 			
 			double stesolid_iv = 0.25*weight;
 			double stesolid_pr = 0.5*weight;
-			setTextViewString(R.id.s_iv_value, String.format(getString(R.string.s_iv_format), stesolid_iv));
-			setTextViewString(R.id.s_pr_value, String.format(getString(R.string.s_pr_format), stesolid_pr));
+			setTextViewText(R.id.s_iv_value, String.format(getString(R.string.s_iv_format), stesolid_iv));
+			setTextViewText(R.id.s_pr_value, String.format(getString(R.string.s_pr_format), stesolid_pr));
 			
 			double albuterol = 2.5;
-			if (age >= 5.0) albuterol = 5.0;
-			setTextViewString(R.id.a2_value, String.format(getString(R.string.a2_format), albuterol));
+			if (age < 0.0)
+				setTextViewText(R.id.a2_value, R.string.unknown_val);
+			else
+			{
+				if (age >= 5.0) albuterol = 5.0;
+				setTextViewText(R.id.a2_value, String.format(getString(R.string.a2_format), albuterol));
+			}
 			
 			double atropine = 0.020 * weight;
-			setTextViewString(R.id.a4_value, String.format(getString(R.string.a4_format), atropine));
+			setTextViewText(R.id.a4_value, String.format(getString(R.string.a4_format), atropine));
 			
 			int amiodarone = 5 * weight;
-			setTextViewString(R.id.a5_value, String.format(getString(R.string.a5_format), amiodarone));
+			setTextViewText(R.id.a5_value, String.format(getString(R.string.a5_format), amiodarone));
+		}
+	}
+	
+	private void updateParametersValues(int weight, double age)
+	{
+		if (findViewById(R.id.parameters_rel_layout) == null)
+			return;
+		
+		if (age < 0.0)
+		{
+			setTextViewText(R.id.breathing_value, R.string.unknown_val);
+			setTextViewText(R.id.heart_value, R.string.unknown_val);
+			setTextViewText(R.id.pressure_value, R.string.unknown_val);
+		}
+		else
+		{
+			int age_index = 3;
+			if (age < 1)
+				age_index = 0;
+			else if (age < 6)
+				age_index = 1;
+			else if (age < 12)
+				age_index = 2;
+			setTextViewText(R.id.breathing_value, getResources().getStringArray(R.array.breathing_values)[age_index]);
+			setTextViewText(R.id.heart_value, getResources().getStringArray(R.array.heart_values)[age_index]);
+			setTextViewText(R.id.pressure_value, getResources().getStringArray(R.array.pressure_values)[age_index]);
 		}
 	}
 
@@ -137,7 +225,7 @@ public class MainActivity extends Activity implements OnCheckedChangeListener, O
 		v.setText(string_id);
 	}
 	
-	private void setTextViewString(int view_id, String s)
+	private void setTextViewText(int view_id, String s)
 	{
 		TextView v = (TextView) findViewById(view_id);
 		v.setText(s);
